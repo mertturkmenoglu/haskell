c83d719e85e992ee4709da0de3636b1a323798c7
Mert <mertturkmenoglu99@gmail.com>
2019-04-30 02:17:50 +0300

    Add mathematical functions example

---
+++ example_004/mathFun.hs
@@ -0,0 +1,18 @@
+import Data.List
+import System.IO
+
+-- Built in math functions
+piValue = pi
+eToThePowerOf9 = exp 9
+logOf9 = log 9
+squareOf9 = 9 ** 2
+truncateValue = truncate 9.999
+roundValue = round 9.999
+ceilValue = ceiling 9.999
+floorValur = floor 9.999
+
+{- sin, cos, tan, asin, atan, acos, sinh
+ - tanh, cosh, asinh, atanh, acosh
+ -}
+
+
