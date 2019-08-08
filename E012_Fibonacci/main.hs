50dc5855853afdf2c9d60b42812e9f047d26aba9
Mert <mertturkmenoglu99@gmail.com>
2019-08-09 00:10:13 +0300

    Update fibonacci example

--- E012_Fibonacci/main.hs
+++ E012_Fibonacci/main.hs
@@ -1,3 +1,10 @@
+fibn n = fibs !! n
+    where
+        fibs = 0 : 1 : map f[2..]
+        f n = fibs!! (n-1) + fibs!!(n-2)
+
 main = do
     let fib = 1 : 1 : [a+b | (a,b) <- zip fib(tail fib)]
     print (take 10 fib)
+
+    print (fibn 10)
