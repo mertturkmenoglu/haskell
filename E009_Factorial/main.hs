3cba1c3b4fc8c837d2e49c430db30ea76987d273
Mert <mertturkmenoglu99@gmail.com>
2019-08-09 00:07:14 +0300

    Update factorial example

--- E009_Factorial/main.hs
+++ E009_Factorial/main.hs
@@ -2,6 +2,9 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+factorial' :: (Integral a) => a -> a
+factorial' n = product [1..n]
+
 main = do
     putStrLn "Please enter n: "
     inp <- getLine
@@ -9,3 +12,6 @@ main = do
     
     let result = factorial n
     putStrLn ("Result: " ++ show result)
+
+    let result2 = factorial' n
+    print (result2)
