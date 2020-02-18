db3a392fb3a5890edab48d326e43cbe9bb7b1245
Mert <mertturkmenoglu99@gmail.com>
2020-02-18 21:29:07 +0300

    Add narcissistic number practice

---
+++ practice/E021_NarcissisticNumber/main.hs
@@ -0,0 +1,11 @@
+-- A narcissistic number is a number which is the sum of its own digits,
+-- each raised to the power of the number of digits in a given base.
+-- Example: [1..9], 153, 370, 371, 407, 1634
+
+import Data.List
+
+isNarcissistic :: Int -> Bool
+isNarcissistic n = n == (sum $ map (^ len) digits)
+    where 
+        digits = map (\x -> read x::Int) $ words $ intersperse ' ' (show n)
+        len = length digits
